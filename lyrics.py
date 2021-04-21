f394445843208089e4968dcfeeaa05dcec475d69
allisondurham <allison.durham@gmail.com>
2021-04-21 17:55:51 -0400

    one function but barely

---
+++ lyrics.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+fly = "I dont know why she swallowed the fly. Perhaps she'll die."
+spider = "She swallowed the spider to catch the fly."
+bird = "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her."
+cat = "She swallowed the cat to catch the bird." 
+dog = "She swallowed the dog to catch the bird."
+goat = "She swallowed the goat to catch the dog."
+cow = "She swallowed the cow to catch the goat."
+horse = "I know an old lady who swallowed a horse. She's dead of course!"
+
+def verse(fly, spider, bird, cat, dog, goat, cow, horse):
+    print(fly)
+
+verse(fly, spider, bird, cat, dog, goat, cow, horse)
+
+
+
+# f"{fly} {spider} {bird}"
+
+# repeated = {"fly" : "I dont know why she swallowed the fly.  Perhaps she'll die.", "spider" : "She swallowed the spider to catch the fly.", "bird" : "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.", "cat" : "She swallowed the cat to catch the bird.", "dog" : "She swallowed the dog to catch the cat.", "goat" : "She swallowed the goat to catch the dog.", "cow" : "She swallowed the cow to catch the goat."}
+# "hello, {fly} {spider} {bird}".format(**repeated)
+
+# i know an old lady who swallowed a (variable).
+# another line or two of rhyming text
+
+
+#   ***  add one each time, but add to the front of list*** 
+
+#She's dead, of course!"
+
+# (new line for horse?)
